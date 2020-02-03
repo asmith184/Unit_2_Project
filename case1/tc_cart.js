1d63bdce575d578b65e4ca6bdaba7398cd4fdd1c
Aspen <asmith184@west-mec.org>
2020-02-03 15:27:53 -0700

    almost done with case 1

--- case1/tc_cart.js
+++ case1/tc_cart.js
@@ -12,3 +12,21 @@
 	
 */
 
+var orderTotal = 0;
+
+//var containing a string of html
+var cartHTML = "<table> <tr> <th>Item</th> <th>Description</th> <th>Price</th> <th>Qty</th> <th>Total</th> </tr>";
+
+//for loop running through all entries in the item array
+for (var i = 0; i < item.length; i++) {
+   cartHTML += "<tr> <td><img src='tc_'" + item[i] + "'.png' alt='item'/></td>";
+   cartHTML += "<td>" + itemDescription[i] + "</td> <td> $" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
+   var itemCost = itemPrice * itemQty[i];
+   cartHTML += "<td> $" + itemCost + "</td></tr>";
+   orderTotal += itemCost; 
+}
+
+cartHTML += "<tr> <td colspan = '4'> Subtotal </td> <td>$" + orderTotal + "</td></tr></table>";
+
+document.getElementById("cart").innerHTML
+
--- case1/tc_order.js
+++ case1/tc_order.js
@@ -17,3 +17,18 @@
    
 */
 
+//array named item containing the id numbers of items purchased 
+var item = [10582, 23015, 41807, 10041];
+
+//array containing item descriptions
+var itemDescription = [
+   "1975 Green Bay Packers Football (signed), Item 10582",
+   "Tom Landry 1955 Football Card (unsigned), Item 23015",
+   "1916 Army-Navy Game, Framed Photo (signed), Item 41807",
+   "Protective Card Sheets, Item 10041"
+];
+
+//array containing quantities that the customer ordered of each item
+var itemQty = [1, 1, 1, 4];
+
+var itemPrice = [149.93, 89.98, 334.93, 22.67]
