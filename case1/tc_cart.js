a34821bcc4c27a0ec293e61d8256db6a622b93d7
Aspen <asmith184@west-mec.org>
2020-02-04 13:08:04 -0700

    finished case 1

--- case1/tc_cart.js
+++ case1/tc_cart.js
@@ -19,14 +19,14 @@ var cartHTML = "<table> <tr> <th>Item</th> <th>Description</th> <th>Price</th> <
 
 //for loop running through all entries in the item array
 for (var i = 0; i < item.length; i++) {
-   cartHTML += "<tr> <td><img src='tc_'" + item[i] + "'.png' alt='item'/></td>";
+   cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='item'/></td>";
    cartHTML += "<td>" + itemDescription[i] + "</td> <td> $" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
-   var itemCost = itemPrice * itemQty[i];
+   var itemCost = itemPrice[i] * itemQty[i];
    cartHTML += "<td> $" + itemCost + "</td></tr>";
    orderTotal += itemCost; 
 }
 
 cartHTML += "<tr> <td colspan = '4'> Subtotal </td> <td>$" + orderTotal + "</td></tr></table>";
 
-document.getElementById("cart").innerHTML
+document.getElementById("cart").innerHTML = cartHTML;
 
